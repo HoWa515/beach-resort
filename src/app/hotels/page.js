3dead84c52a63632e6a579ca44c746bcc1d3897f
wh <sunfortea123@gmail.com>
2025-09-06 17:51:21 +0200

    routes

---
+++ src/app/hotels/page.js
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1>Page hotels</h1>;
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,10 @@
+import Nav from "./components/Nav";
+
 export default function Home() {
-  return <h1>Hello Next</h1>;
+  return (
+    <>
+      <h1>Welcome to beach resort</h1>
+      <Nav />
+    </>
+  );
 }
