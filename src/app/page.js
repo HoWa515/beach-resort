df6bebf8b88ccc570cfb26b44daf8859534ee299
wh <sunfortea123@gmail.com>
2025-09-08 13:54:58 +0200

    logo and layout

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,7 @@ import Nav from "./components/Nav";
 export default function Home() {
   return (
     <>
-      <h1>Welcome to beach resort</h1>
-      <Nav />
+      <h1>Welcome to beach club!</h1>
     </>
   );
 }
