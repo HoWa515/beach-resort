8529a53ccb2434c9788ae25787aaf516b233ac6b
wh <sunfortea123@gmail.com>
2025-10-05 16:09:39 +0200

    minor

--- src/app/components/Logo.js
+++ src/app/components/Logo.js
@@ -1,9 +1,9 @@
 function Logo() {
   return (
     <a href="/" className="flex items-center gap-4 z-10">
-      <img src="./logo.jpg" height="60" width="60" alt="The Wild Oasis logo" />
+      <img src="./logo.jpg" height="60" width="60" alt="The logo" />
       <span className="text-xl font-semibold text-primary-100">
-        The Wild Oasis
+        The Beach Club
       </span>
     </a>
   );
